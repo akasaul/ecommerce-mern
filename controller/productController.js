9c0fc81822596d299451050bbcba37daf4c2c90a
Nikodimos Jemaneh <nikodimosjemaneh40@gmail.com>
2023-02-21 04:01:46 -0500

    add and get product routes  with validation at the backend

--- controller/productController.js
+++ controller/productController.js
@@ -1,11 +1,103 @@
+const asyncHandler = require('express-async-handler');
 const Product = require('../model/Product');
+const {validationResult} = require('express-validator');
 
-const addProduct = (req, res, next) => {
-    res.send('add product');
-    next();
-}
+const addProduct = asyncHandler(
+    async (req, res) => {
+        const {name, price, desc, category, qty, imageUrl } = req.body;
+        const errors = validationResult(req);
+        let product;
+
+        if(!name || !price || !desc || !qty || !imageUrl) {
+            res.status(400);
+            throw new Error('Please Provide the required fields')    
+        }
+
+        if(!errors.isEmpty()) {
+            res.status(400);
+            throw new Error(errors.array()[0].msg);
+        }
+
+        try {
+            product = await Product.create({
+                name, price, category, qty, description: desc, postedBy: req.user._id, imageUrl
+            })
+        } catch(err) {
+            res.status(500);
+            throw new Error('Failed to Create product');
+        }
+        res.status(200).json(product);
+    }
+)  
+
+const getProducts = asyncHandler(
+    async (req, res) => {
+        const currentPage = Number(req.query.page);
+
+        const productsPerPage = 10;
+        const skip = (currentPage - 1) * productsPerPage;
+
+        try {
+            const products = await Product.find()
+                .skip(skip)
+                .limit(productsPerPage)
+                .exec()
+
+                res.status(200).json(products);
+        } catch(err) {
+            res.status(500);
+            throw new Error('Failed to fetch products');
+        }
+    }
+)
+
+const getProduct = asyncHandler(
+    async (req, res) => {
+        const {prodId} = req.params;
+        try {
+            const product = await Product.findById(prodId).populate('postedBy');
+            if(!product) {
+                throw new Error('Product not Found');
+            }
+            res.status(200).json(product);
+        } catch(err) {
+            res.status(500);
+            throw new Error('Failed to fetch the product');
+        }
+    }
+)
+
+const updateProduct = asyncHandler(
+    async (req, res) => {
+        const {prodId} = req.params;
+        const {name, price, desc} = req.body;
+
+        const errors = validationResult(req);
+        let product;
+
+        if(!name || !price || !desc) {
+            res.status(400);
+            throw new Error('Please Provide all the fields')    
+        }
+
+        if(!errors.isEmpty()) {
+            res.status(400);
+            throw new Error(errors.array()[0].msg);
+        }
+
+        try {
+            product.name = name;
+            product.price = price;
+            product.desc = desc;
+            const newProduct = await product.save();
+            res.status(200).json(newProduct);
+        } catch(err) {
+            res.status(500);
+            throw new Error('Failed to Update product');
+        }
+        res.status(200).json(product);
+    }
+)  
 
 
-module.exports = {
-    addProduct
-};
+module.exports = {addProduct, getProducts, getProduct, updateProduct}
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,8 +1,29 @@
-const { addProduct } = require('../controller/productController');
+const express = require('express');
+const router = express.Router();
 const protect = require('../middleware/protect');
+const {body} = require('express-validator')
 
-const router = require('express').Router();
+const { addProduct, getProducts, getProduct, updateProduct, getCart, addToCart} = require('../controller/productController');
 
-router.post('/add', protect, addProduct)
+router.post('/add-product', [
+    body('name').isLength({min: 5}).withMessage('Name Must be at least 5 characters').isAlphanumeric('en-US', {ignore: ' '}).withMessage('Name cant contain special characters'),
+    body('price').isNumeric().withMessage('Price Should only be in Numbers').isFloat({min: 1}).withMessage('Price cant be less that 0$'),
+    body('desc').isLength({min: 5, max: 100}).withMessage('Description Must be between 5 and 100 characters'),
+    body('category').isIn(['food', 'other', 'instrument', 'cloth', 'furniture', 'electronics']).withMessage('Invalid category Provided'),
+    body('qty').isNumeric().withMessage('Quantity Should be in Numbers').isFloat({min: 1}).withMessage('Price cant be less that 0$'),
+    body('imageUrl').isURL().withMessage('Enter a valid URL')
+], protect, addProduct);
+
+router.get('/', getProducts);
+
+router.route('/:prodId').get(getProduct).post([
+    body('name').isLength({min: 5}).withMessage('Name Must be at least 5 characters').isAlphanumeric('en-US', {ignore: ' '}).withMessage('Name cant contain special characters'),
+    body('price').isNumeric().withMessage('Price Should only be in Numbers').isFloat({min: 0}).withMessage('Price cant be less that 0$'),
+    body('desc').isLength({min: 5, max: 100}).withMessage('Description Must be between 5 and 100 characters'),
+    body('category').isIn(['food', 'other', 'instrument', 'cloth', 'furniture', 'electronics']).withMessage('Invalid category Provided'),
+    body('qty').isNumeric().withMessage('Quantity Should be in Numbers').isFloat({min: 0}).withMessage('Price cant be less that 0$'),
+    body('imageUrl').isURL().withMessage('Enter a valid URL')
+], protect, updateProduct);
+
 
 module.exports = router;
